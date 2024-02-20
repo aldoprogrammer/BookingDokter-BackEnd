14e1fb99101ad7f2531dc9990b702e6d6baf5353
aldoprogrammer <aldobesma@gmail.com>
2024-02-20 15:26:28 +0700

    update

--- Controllers/bookingController.js
+++ Controllers/bookingController.js
@@ -4,55 +4,60 @@ import Booking from '../models/BookingSchema.js'
 import Stripe from 'stripe'
 
 export const getCheckoutSession = async (req, res) => {
+    
     try {
-        
         const doctor = await Doctor.findById(req.params.doctorId);
         const user = await User.findById(req.userId);
 
+        if (!doctor || isNaN(doctor.ticketPrice)) {
+            throw new Error('Invalid doctor or ticket price');
+        }
+
         const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-        const session = await stripe.checkout.sessions.create({
+        session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             mode: 'payment',
             success_url: `${process.env.CLIENT_SITE_URL}/checkout-success`,
             cancel_url: `${req.protocol}://${req.get('host')}/doctors/${doctor.id}`,
             customer_email: user.email,
-            client_reference_id: doctor.id,
+            client_reference_id: req.params.doctorId,
             line_items: [
                 {
                     price_data: {
                         currency: 'bdt',
-                        unit_amount: doctor.ticketPrice * 100,
+                        unit_amount: doctor.ticketPrice * 100, 
                         product_data: {
-                            name: doctor.name,
-                            description: doctor.bio,
-                            images: [doctor.photo]
+                            name:doctor.name,
+                            description:doctor.bio,
+                            images:[doctor.photo]
                         }
                     },
                     quantity: 1
                 }
             ]
-        })
+        });
 
         const booking = new Booking({
-            doctor: doctor._id,
-            user: user._id,
-            ticketPrice: doctor.ticketPrice,
-            session: session.id
-        })
+            doctor:doctor._id,
+            user:user._id,
+            ticketPrice:doctor.ticketPrice,
+            session:session.id
+        });
 
         await booking.save();
-        res.send(200).json({
+        res.status(200).json({
             success: true,
             message: 'Paid successfully',
             session
-        })
+        });
 
     } catch (err) {
-        res.send(500).json({
+        console.log(err)
+        res.status(500).json({
             success: false,
-            message: 'Error happend',
-            session
-        })
+            message: err.message,
+            
+        });
     }
 }
